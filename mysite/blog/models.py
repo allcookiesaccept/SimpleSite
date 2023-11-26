55fc7ffc2e70ecf060690c7cb99b5a70ee28b379
allcookiesaccept <insufferable.lobster@gmail.com>
2023-11-26 22:17:12 +0300

    first templates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,32 +1,38 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
 # Create your models here.
 class Post(models.Model):
-
-
-
     class Meta:
-        __default_sorting = '-publish'
+        __default_sorting = "-publish"
         ordering = [__default_sorting]
-        indexes = [
-            models.Index(fields=[__default_sorting])
-        ]
-
+        indexes = [models.Index(fields=[__default_sorting])]
 
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB', 'Published'
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
 
-
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
+    objects = models.Manager()  # менеджер, применяемый по умолчанию
+    published = PublishedManager()
 
     def __str__(self):
         return self.title
